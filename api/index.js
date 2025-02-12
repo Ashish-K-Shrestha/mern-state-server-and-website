3200ba5a33b987dedd6aa62704afe465aad6e505
Ashish K. Shrestha <ashish.ashishshrestha@gmail.com>
2025-02-13 03:01:16 +0545

    updates

---
+++ api/index.js
@@ -0,0 +1,17 @@
+ import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Conneted to MongoDB");
+ }).catch((err) => {
+    console.log("Error:"+err);
+ });
+
+
+ const app = express();
+
+ app.listen(3000, () => {
+    console.log("Server is running on port 3000 !!");
+ }); 
