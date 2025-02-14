3e710c10d8d239b8f25919df46af62495b55f1cf
Ashish K. Shrestha <ashish.ashishshrestha@gmail.com>
2025-02-14 14:11:55 +0545

    added auth, signup, error backend

--- api/index.js
+++ api/index.js
@@ -1,20 +1,36 @@
- import dotenv from "dotenv";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Conneted to MongoDB");
- }).catch((err) => {
-    console.log("Error:"+err);
- });
+  })
+  .catch((err) => {
+    console.log("Error:" + err);
+  });
 
+const app = express();
+app.use(express.json());
 
- const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 !!");
+});
 
- app.listen(3000, () => {
-    console.log("Server is running on port 3000 !!");
- }); 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
- app.use('/api/user', userRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
